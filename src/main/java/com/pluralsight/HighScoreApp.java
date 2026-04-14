1bd31bc25cbdf9c435244e5fa1653d1c1e1477ca
itkhari <mrkharij1@gmail.com>
2026-04-14 11:58:17 -0400

    made scoreboard

--- src/main/java/com/pluralsight/HighScoreApp.java
+++ src/main/java/com/pluralsight/HighScoreApp.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class HighScoreApp {
     public static void main(String[] args) {
-        
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter game score");
+    System.out.println("Format: Home:Visitor|0:0");
+    System.out.print("Enter here: ");
+    String finalScore = sc.nextLine();
+
+    String[] splitInput = finalScore.split(Pattern.quote("|"));
+    String teamName = splitInput[0];
+    String[] teams = teamName.split(Pattern.quote(":"));
+
+    String teamscores = splitInput[1];
+    String[] scores = teamscores.split(Pattern.quote(":"));
+    int home = Integer.parseInt(scores[0]);
+    int away = Integer.parseInt(scores[1]);
+
+    if (home > away) {
+        System.out.println("Winner: Home");
+    } else if (away > home) {
+        System.out.println("Winner: Visitor");
+    }
+    if (home == away) {
+        System.out.println("Game ends in tie");
+    }
+
+
     }
 }
